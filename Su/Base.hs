af2be581994293402e741862946ebb422661a25c
Spencer Gordon <sgord512@gmail.com>
2012-06-26 01:25:56 +0200

    refactoring, added command line, prepped for optimization to come

--- Su/Base.hs
+++ Su/Base.hs
@@ -1,188 +1,209 @@
+{-# LANGUAGE RecordWildCards #-}
 module Su.Base where
 
 import Control.Applicative
+import Control.Monad.State
 import Data.List
+import Data.Maybe ( fromJust )
 import Data.Map ( Map )
 import qualified Data.Map as Map
-import Data.Maybe ( fromJust )
-import Util.List ( chunk )
-import Util.String
+import System.Console.GetOpt
+import System.Environment
 import System.Random
+import Su.Types
+import Util.List
+import Util.Monad
 
-data Solver = Solver { boardS :: Board, 
-                       genS :: StdGen, 
-                       problemsS :: Problems, 
-                       movesS :: Moves,
-                       deadEndsS :: DeadEnds
-                       
-                     }
+type GameState = State Solver
 
-data DeadEnd = DeadEnd Square Integer Problems 
+rGen f = do state <- get
+            let (a, gen') = f $ genS state
+            put $ state { genS = gen' }
+            return a
 
-instance Show DeadEnd where
-  show (DeadEnd s i ps) = "Square at " ++ (show $ loc s) ++ " cannot be filled with " ++ (show i) ++ " because " ++ (concat $ intersperse ", and " (map show ps))
- 
-type DeadEnds = [DeadEnd]
+randomElement :: [a] -> GameState a
+randomElement xs = do 
+  ix <- rGen $ randomR (0, length xs - 1)
+  return (xs !! ix)
 
-data Move = SolutionApplication Solution | RandomMove Square [Integer]
+possibilities :: Square -> GameState [Integer]
+possibilities s = do 
+  sqs <- liftM concat $ mapM squaresInRegion (regions s)
+  let prelimPoss = nums \\ (nub $ map val (filter filled sqs))
+  des <- deadEnds s
+  return $ prelimPoss \\ des
 
-instance Show Move where
-  show (SolutionApplication soln) = "Applied solution: " ++ show soln
-  show (RandomMove sq poss) = "Randomly filled in square " ++ (show sq)
+deadEnds :: Square -> GameState [Integer]
+deadEnds s = do
+  allDeadEnds <- gets ((Map.lookup (loc s)) . deadEndsS)
+  let dEnds = case allDeadEnds of 
+        Nothing -> []
+        Just des -> map (\(DeadEnd s i _) -> i) des
+  return dEnds
+
+isSolvable :: Square -> GameState Bool
+isSolvable s = do 
+  poss <- possibilities s
+  return (length poss == 1)
+  
+isUnsolvable :: Square -> GameState Bool  
+isUnsolvable s = do
+  poss <- possibilities s
+  return $ null poss
+  
+allSquareSolutions :: GameState [Solution]
+allSquareSolutions = do squares <- gets (allSquares . boardS) 
+                        let possSquares = filter unfilled squares
+                        filterMapM hasSolution possSquares
+                        
+hasSolution :: Square -> GameState (Maybe Solution)  
+hasSolution sq = do 
+  poss <- possibilities sq
+  case poss of
+    (n:[]) -> return $ Just $ Solution (Square (loc sq) (Just n)) [OnlyAllowableValue]
+    _ -> return Nothing
+
+squaresInRegion :: Region -> GameState [Square]
+squaresInRegion r@(Region s i) = do 
+  squares <- gets (allSquares . boardS)
+  return $ filter ((== r) . (getShapeFunc s)) squares
+  
+numbersToMatch :: Region -> GameState [Integer]
+numbersToMatch r = do
+  sqs <- squaresInRegion r
+  return $ nums \\ (map val (filter filled sqs))
+  
+isPossibleValueFor :: Integer -> Square -> GameState Bool
+i `isPossibleValueFor` s = do
+  poss <- possibilities s
+  return $ i `elem` poss
+  
+squaresForNumbers :: Region -> GameState [(Integer, [Square])]
+squaresForNumbers r = do
+  sqs <- squaresInRegion r
+  let emptySquares = filter unfilled sqs  
+      numsToMatch = nums \\ (map val (filter filled sqs))
+  (liftM $ zip numsToMatch) (forM numsToMatch (\n -> filterM (n `isPossibleValueFor`) emptySquares))
+
+regionSolutions :: Region -> GameState [Solution]
+regionSolutions r = do 
+  sqWithNums <- squaresForNumbers r
+  return $ filterMap (\(n, sqs) -> if length sqs == 1 
+                                   then Just $ Solution (Square (loc (head sqs)) (Just n)) [OnlyLocationForNumberInRegion r]
+                                   else Nothing) sqWithNums
+
+unmatchableNumbers :: Region -> GameState [Problem]
+unmatchableNumbers r = do
+  sqsWithNums <- squaresForNumbers r
+  return $ filterMap (\(n, sqs) -> if null sqs then Just $ NumberUnmatchable r n else Nothing) sqsWithNums
+
+fillRandomSquare :: GameState Move
+fillRandomSquare = do 
+  sq <- (liftM $ filter unfilled) (gets $ allSquares . boardS) >>= randomElement
+  poss <- possibilities sq
+  value <- randomElement poss
+  sq' <- updateSquare $ Square (loc sq) (Just value)
+  return $ RandomMove sq' poss
+  
+allRegionSolutions :: GameState [Solution]  
+allRegionSolutions = do
+  rgns <- gets (allRegions . boardS)
+  rgnSolutions <- mapM regionSolutions rgns
+  return $ foldr1 (\a b -> union a b) rgnSolutions
+  
+allSolutions :: GameState [Solution]
+allSolutions = do
+  sqSolutions <- allSquareSolutions
+  reSolutions <- allRegionSolutions
+  let solnPairs = map (\soln@(Solution sq _) -> (sq, soln)) (sqSolutions ++ reSolutions)
+  return $ (snd . unzip) $ Map.toList $ Map.fromListWith (\soln soln' -> Solution (solutionSquare soln) (reasons soln ++ reasons soln')) solnPairs
+        
+fillSolvedSquare :: Solution -> GameState Move
+fillSolvedSquare soln@(Solution s _) = do updateSquare s 
+                                          return $ SolutionApplication soln
+                                                              
+updateSquare :: Square -> GameState Square
+updateSquare sq = modify (\solver@Solver { boardS = (Board map regions) } -> solver { boardS = Board (Map.insert (loc sq) sq map) regions }) >> return sq
+  
+boardUnfilled = do 
+  sqs <- gets (allSquares . boardS)
+  return $ not $ null (filter unfilled sqs)
+
+                 
+solveIO :: Solver -> IO Solver         
+solveIO solver = do
+  let (continue, solver') = runState boardUnfilled solver
+      (probs, solver'') = runState problems solver'
+  if continue
+    then if null probs
+         then do solver''' <- oneStepIO solver''
+                 putStrLn $ display solver'''
+                 solveIO solver'''
+         else do mapM_ (putStrLn . display) probs
+                 let solver''' = execState (markDeadEnd >> undoLastMove) solver'' 
+                 solveIO solver'''
+    else do putStrLn "Done!" >> return solver''
+          
+oneStepIO solver = do              
+  let (solns, solver') = runState allSolutions solver
+  mapM_ print solns
+  let (move, solver'') = case solns of
+        [] -> runState fillRandomSquare solver'
+        soln:solns -> runState (fillSolvedSquare soln) solver'
+  return $ execState (recordMove move) solver''
               
-squareInMove :: Move -> Square                                           
-squareInMove (SolutionApplication (Solution sq _)) = sq
-squareInMove (RandomMove sq _) = sq                                                    
-                                           
-instance Display Solver where
-  display Solver{ movesS = moves, boardS = board } = displayColor (if null moves then Nothing else Just $ squareInMove $ head moves) board
+solve :: GameState Solver
+solve = do 
+  continue <- boardUnfilled
+  if continue      
+     then do probs <- problems 
+             if null probs 
+               then do oneStep 
+                       solve
+               else do markDeadEnd
+                       undoLastMove
+                       solve
+    else get 
+
+oneStep = do
+  solns <- allSolutions
+  move <- case solns of
+    [] -> fillRandomSquare              
+    x:xs -> fillSolvedSquare x               
+  recordMove move
+          
+recordMove :: Move -> GameState Move
+recordMove m = modify (\s@Solver{ movesS = moves } -> s { movesS = (m:moves) }) >> return m
+    
+markDeadEnd :: GameState ()
+markDeadEnd = do 
+  s <- get
+  let sq = (squareInMove $ head $ movesS s)
+      problems = problemsS s
+      de = DeadEnd sq (val sq) problems
+  modify (\solver@Solver { deadEndsS = deadEnds } -> solver { deadEndsS = Map.insertWith (++) (loc sq) [de] deadEnds })
   
-displayColor lastMove b = let board = map (\row -> " " ++ (concat $ displayRow $ map (displayWithHighlighting lastMove) row) ++ " ") (to2dArray $ allSquares b)
-                              sqs = splitRows board
-                          in "\n" ++ (concat $ intersperse "\n" sqs) ++ "\n"
-                          
-type Moves = [Move]
-type Problems = [Problem]              
-
-data Solution = Solution Square Reason deriving Eq
-
-instance Show Solution where
-  show (Solution sq reason) = "Square at " ++ show (loc sq) ++ " can be filled in with " ++ show (val sq) ++ " because " ++ show reason
-
-data Reason = OnlyAllowableValue | OnlyLocationForNumberInRegion Region deriving Eq
-
-instance Show Reason where
-  show OnlyAllowableValue = "all other values appeared already in its row, box, and column"
-  show (OnlyLocationForNumberInRegion region) = "this number cannot be placed in any other square in " ++ show region
-
-initialState seed = Solver { boardS = newBoard, 
-                             genS = mkStdGen seed, 
-                             problemsS = [], 
-                             movesS = [], 
-                             deadEndsS = [] }
-
-class Display a where
-  display :: a -> String
-
-instance Display a => Display [a] where 
-  display [] = ""
-  display (x:xs) = display x ++ (display xs)
-
-data Square = Square Loc (Maybe Integer)
-
-instance Eq Square where
-  s == s' = loc s == loc s'
-
-displayWithHighlighting :: Maybe Square -> Square -> String
-displayWithHighlighting Nothing sq = display sq
-displayWithHighlighting (Just sqMove) sq | sqMove == sq = color Green (display sq)
-                                         | otherwise = display sq
-
-instance Display Square where 
-  display (Square _ (Just v)) = (show v)
-  display (Square _ Nothing) = [boxChar]
+undoLastMove :: GameState ()
+undoLastMove = do
+  (m:ms) <- gets movesS
+  let sq = squareInMove m
+  updateSquare (Square (loc sq) Nothing)
+  modify (\s -> s { movesS = ms })
   
-instance Show Square where
-  show (Square l (Just v)) = "Square at " ++ (show l) ++ " filled in with " ++ (show v)
-  show (Square l Nothing) = "Square at " ++ (show l) ++ " currently empty"
+               
+stepBoard :: Solver -> Solver
+stepBoard solver = execState oneStep solver
 
-data Problem = SquareUnfillable Square | NumberUnmatchable Region Integer | SquareWithConflictingSolutions Square [Integer]
+problems :: GameState [Problem]
+problems = do
+  sqs <- (liftM (filter unfilled)) (gets (allSquares . boardS))
+  sqs' <- filterMapM (\s -> do unsolvable <- isUnsolvable s; if unsolvable then return $ Just (SquareUnfillable s) else return Nothing) sqs
+  multipleSolns <- filterMapM (\(Solution sq _) -> do squareMap <- gets (squaresMap . boardS)
+                                                      let actualSquare = fromJust $ Map.lookup (loc sq) squareMap
+                                                      if filled actualSquare 
+                                                        then return $ Just (SquareWithConflictingSolutions sq (nub [val sq, val actualSquare]))
+                                                        else return Nothing) =<< allSolutions
+  rgns <- gets (allRegions . boardS)
+  unmatchableNums <- (liftM concat) ((mapM unmatchableNumbers) rgns)
+  return $ sqs' ++ unmatchableNums ++ multipleSolns
 
-instance Display Problem where 
-  display p = "Problem: " ++ show p
-
-instance Show Problem where
-  show (SquareUnfillable s) = "Square at " ++ (show $ loc s) ++ " cannot be filled!"
-  show (NumberUnmatchable r i) = "There is no place to put a " ++ (show i) ++ " in " ++ (display r) ++ "!"
-  show (SquareWithConflictingSolutions s ls) = "Square at " ++ (show $ loc s) ++ " must be all of the following: " ++ (show ls)
-data Loc = Loc (Integer, Integer) deriving Eq
-r (Loc (a, _)) = a
-c (Loc (_, b)) = b
-
-instance Show Loc where
-  show (Loc (r, c)) = "(" ++ (show r) ++ "," ++ (show c) ++ ")"
-
-instance Ord Loc where 
-  (Loc (row, col)) `compare` (Loc (row', col')) = 
-    case row `compare` row' of
-      EQ -> col `compare` col'
-      neq -> neq
-      
-instance Display Loc where
-  display (Loc l) = show l
-
-data Region = Region Shape Integer deriving Eq
-
-instance Show Region where
-  show = display
-
-instance Display Region where
-  display (Region s n) = show s ++ " " ++ show n
-
-data Board = Board SquaresMap Regions deriving Show
-
-newBoard :: Board
-newBoard = let squares = [Square (Loc (row, col)) Nothing | row <- nums, col <- nums ]
-               regions = nums <**> (map (\s n -> Region s n) (map fst shapeFuncs))
-           in Board (Map.fromList $ zip (map loc squares) squares) regions
-              
-
-
-instance Display Board where 
-  display b = let board = map (\row -> " " ++ (concat $ displayRow $ map display row) ++ " ") (to2dArray $ allSquares b)
-                  sqs = splitRows board
-              in (concat $ intersperse "\n" sqs) ++ "\n"
-
-displayRow :: [String] -> [String]                 
-displayRow row = intersperse " " (intercalate ["|"] (chunk 3 row))
-
-splitRows :: [String] -> [String]
-splitRows arr = intercalate [replicate 24 '-'] (chunk 3 arr)                 
-
-to2dArray :: [Square] -> [[Square]]
-to2dArray ls = groupBy (\s s' -> (r $ loc s) == (r $ loc s')) ls
-
-data Shape = Row | Col | Box deriving (Show, Bounded, Enum, Ord, Eq)
-
-type Squares = [Square]
-type SquaresMap = Map Loc Square
-type Regions = [Region]
-  
-row (Square (Loc (r, _)) _) = Region Row r
-col (Square (Loc (_, c)) _) = Region Col c
-box (Square (Loc (r, c)) _) = Region Box (((r - 1) `quot` 3) * 3 + ((c - 1) `quot` 3) + 1) 
-
-loc :: Square -> Loc
-loc (Square l _) = l
-
-filled :: Square -> Bool
-filled (Square _  Nothing)  = False
-filled (Square _ (Just _))  = True
-
-unfilled = not . filled
-
-val :: Square ->  Integer
-val (Square loc v) = fromJust v
-
-shapes = [Row, Col, Box]
-funcs = [row, col, box]
-shapeFuncs = zip shapes funcs
-getShapeFunc s = fromJust $ lookup s shapeFuncs
-
-shape (Region s _) = s
-num (Region _ n) = n
-
-allSquares (Board s _) = Map.elems s
-
-squaresMap (Board s _) = s
-allRegions (Board _ r) = r
-
-rows b = filter ((== Row) . shape) (allRegions b)
-cols b = filter ((== Col) . shape) (allRegions b)
-boxes b = filter ((== Box) . shape) (allRegions b)
-
-regions :: Square -> Regions
-regions = (<**> [row, col, box]) . pure
-  
-boxChar = '\x2B1C';
-nums = [1..9]
--- sudoku.hs
+++ sudoku.hs
@@ -1,214 +1,51 @@
-{-# LANGUAGE RecordWildCards #-}
 module Main where
 
-import Control.Applicative
+import Control.Applicative ( (<*>) )
+import Control.Monad ( join )
 import Control.Monad.State
-import Data.List
-import Data.Maybe ( fromJust )
-import Data.Map ( Map )
-import qualified Data.Map as Map
+import Data.List ( find )
+import Data.Maybe ( fromJust, isJust, maybe )
+import Su.Base
+import Su.Types
+import System.Console.GetOpt
 import System.Environment
 import System.Random
-import Su.Base
-import Util.List
-import Util.Monad
 
-type GameState = State Solver
+data Flag = Verbose | Seed Int
+isSeed :: Flag -> Bool
+isSeed (Seed _) = True
+isSeed _ = False
 
-rGen f = do state <- get
-            let (a, gen') = f $ genS state
-            put $ state { genS = gen' }
-            return a
+isVerbose :: Flag -> Bool
+isVerbose Verbose = True
+isVerbose _ = False
 
-randomElement :: [a] -> GameState a
-randomElement xs = do 
-  ix <- rGen $ randomR (0, length xs - 1)
-  return (xs !! ix)
+options :: [OptDescr Flag]
+options = 
+  [ Option ['v'] ["verbose"] (NoArg Verbose) "shows intermediate output, and detailed information about the solving/generating process"
+  , Option ['s'] ["seed"] (ReqArg (Seed . read) "INTEGER") "choose a seed for the random number generator; the same seed will generate the same board"
+  ]
 
-possibilities :: Square -> GameState [Integer]
-possibilities s = do 
-  sqs <- liftM concat $ mapM squaresInRegion (regions s)
-  let prelimPoss = nums \\ (nub $ map val (filter filled sqs))
-  des <- deadEnds s
-  return $ prelimPoss \\ des
-
-deadEnds :: Square -> GameState [Integer]
-deadEnds s = do
-  allDeadEnds <- gets deadEndsS
-  let dEnds = filterMap (\(DeadEnd sq i _) -> if sq == s then Just i else Nothing) allDeadEnds
-  return dEnds
-
-isSolvable :: Square -> GameState Bool
-isSolvable s = do 
-  poss <- possibilities s
-  return (length poss == 1)
+config :: IO (Bool, Int)
+config = do
+  args <- getArgs
+  let opts = case getOpt RequireOrder options args of
+        (_, _, e:es) -> error "Invalid command line arguments"
+        (o, _, _) -> o
+      verbose = any isVerbose opts
+      maybeSeed = find isSeed opts
+  seed <- case maybeSeed of
+    Nothing -> getStdRandom random
+    Just (Seed i) -> return i
+  return (verbose, seed)
   
-isUnsolvable :: Square -> GameState Bool  
-isUnsolvable s = do
-  poss <- possibilities s
-  return $ null poss
-  
-allSquareSolutions :: GameState [Solution]
-allSquareSolutions = do squares <- gets (allSquares . boardS) 
-                        let possSquares = filter unfilled squares
-                        filterMapM hasSolution possSquares
-                        
-hasSolution :: Square -> GameState (Maybe Solution)  
-hasSolution sq = do 
-  poss <- possibilities sq
-  case poss of
-    (n:[]) -> return $ Just $ Solution (Square (loc sq) (Just n)) OnlyAllowableValue
-    _ -> return Nothing
-
-squaresInRegion :: Region -> GameState [Square]
-squaresInRegion r@(Region s i) = do 
-  squares <- gets (allSquares . boardS)
-  return $ filter ((== r) . (getShapeFunc s)) squares
-  
-numbersToMatch :: Region -> GameState [Integer]
-numbersToMatch r = do
-  sqs <- squaresInRegion r
-  return $ nums \\ (map val (filter filled sqs))
-  
-isPossibleValueFor :: Integer -> Square -> GameState Bool
-i `isPossibleValueFor` s = do
-  poss <- possibilities s
-  return $ i `elem` poss
-  
-squaresForNumbers :: Region -> GameState [(Integer, [Square])]
-squaresForNumbers r = do
-  sqs <- squaresInRegion r
-  let emptySquares = filter unfilled sqs  
-      numsToMatch = nums \\ (map val (filter filled sqs))
-  (liftM $ zip numsToMatch) (forM numsToMatch (\n -> filterM (n `isPossibleValueFor`) emptySquares))
-
-regionSolutions :: Region -> GameState [Solution]
-regionSolutions r = do 
-  sqWithNums <- squaresForNumbers r
-  return $ filterMap (\(n, sqs) -> if length sqs == 1 
-                                   then Just $ Solution (Square (loc (head sqs)) (Just n)) (OnlyLocationForNumberInRegion r)
-                                   else Nothing) sqWithNums
-
-unmatchableNumbers :: Region -> GameState [Problem]
-unmatchableNumbers r = do
-  sqsWithNums <- squaresForNumbers r
-  return $ filterMap (\(n, sqs) -> if null sqs then Just $ NumberUnmatchable r n else Nothing) sqsWithNums
-
-fillRandomSquare :: GameState Move
-fillRandomSquare = do 
-  sq <- (liftM $ filter unfilled) (gets $ allSquares . boardS) >>= randomElement
-  poss <- possibilities sq
-  value <- randomElement poss
-  sq' <- updateSquare $ Square (loc sq) (Just value)
-  return $ RandomMove sq' poss
-  
-allRegionSolutions :: GameState [Solution]  
-allRegionSolutions = do
-  rgns <- gets (allRegions . boardS)
-  rgnSolutions <- mapM regionSolutions rgns
-  return $ foldr1 (\a b -> union a b) rgnSolutions
-  
-allSolutions :: GameState [Solution]
-allSolutions = do
-  sqSolutions <- allSquareSolutions
-  reSolutions <- allRegionSolutions
-  return $ union sqSolutions reSolutions
-
-fillSolvedSquare :: Solution -> GameState Move
-fillSolvedSquare soln@(Solution s _) = do updateSquare s 
-                                          return $ SolutionApplication soln
-                    
-                    
-updateSquare :: Square -> GameState Square
-updateSquare sq = modify (\solver@Solver { boardS = (Board map regions) } -> solver { boardS = Board (Map.insert (loc sq) sq map) regions }) >> return sq
-  
-boardUnfilled = do 
-  sqs <- gets (allSquares . boardS)
-  return $ not $ null (filter unfilled sqs)
-
-solve :: GameState Solver
-solve = do 
-  continue <- boardUnfilled
-  if continue      
-     then do probs <- problems 
-             if null probs 
-               then do oneStep 
-                       solve
-               else do markDeadEnd
-                       undoLastMove
-                       solve
-    else get 
-                 
-solveIO :: Solver -> IO Solver         
-solveIO solver = do
-  let (continue, solver') = runState boardUnfilled solver
-      (probs, solver'') = runState problems solver'
-  if continue
-    then if null probs
-         then do solver''' <- oneStepIO solver''
-                 putStrLn $ display solver'''
-                 solveIO solver'''
-         else do let solver''' = execState (markDeadEnd >> undoLastMove) solver'' -- mapM_ (putStrLn . display) probs
-                 solveIO solver'''
-    else do putStrLn "Done!" >> return solver''
-          
-oneStepIO solver = do              
-  let (solns, solver') = runState allSolutions solver
-  mapM_ print solns
-  let (move, solver'') = case solns of
-        [] -> runState fillRandomSquare solver'
-        soln:solns -> runState (fillSolvedSquare soln) solver'
-  return $ execState (recordMove move) solver''
-              
-oneStep = do
-  solns <- allSolutions
-  move <- case solns of
-    [] -> fillRandomSquare              
-    x:xs -> fillSolvedSquare x               
-  recordMove move
-          
-recordMove :: Move -> GameState ()
-recordMove m = modify (\s@Solver{ movesS = moves } -> s { movesS = (m:moves) })
-    
-markDeadEnd :: GameState ()
-markDeadEnd = do 
-  s <- get
-  let sq = (squareInMove $ head $ movesS s)
-      problems = problemsS s
-      de = DeadEnd sq (val sq) problems
-  put $ s { deadEndsS = (de: deadEndsS s) }
-  
-undoLastMove :: GameState ()
-undoLastMove = do
-  (m:ms) <- gets movesS
-  let sq = squareInMove m
-  updateSquare (Square (loc sq) Nothing)
-  modify (\s -> s { movesS = ms })
-  
-               
-stepBoard :: Solver -> Solver
-stepBoard solver = execState oneStep solver
-
-problems :: GameState [Problem]
-problems = do
-  sqs <- (liftM (filter unfilled)) (gets (allSquares . boardS))
-  sqs' <- filterMapM (\s -> do unsolvable <- isUnsolvable s; if unsolvable then return $ Just (SquareUnfillable s) else return Nothing) sqs
-  multipleSolns <- filterMapM (\(Solution sq _) -> do squareMap <- gets (squaresMap . boardS)
-                                                      let actualSquare = fromJust $ Map.lookup (loc sq) squareMap
-                                                      if filled actualSquare 
-                                                        then return $ Just (SquareWithConflictingSolutions sq (nub [val sq, val actualSquare]))
-                                                        else return Nothing) =<< allSolutions
-  rgns <- gets (allRegions . boardS)
-  unmatchableNums <- (liftM concat) ((mapM unmatchableNumbers) rgns)
-  return $ sqs' ++ unmatchableNums ++ multipleSolns
-
-seedValue = 1000
 
 main = do 
-  let game = initialState seedValue
-  putStrLn ("Seed is: " ++ show seedValue)      
+  (verbose, seed) <- config
+  let game = initialState seed
+  putStrLn $ "Seed is: " ++ show seed
   args <- getArgs 
-  solved <- case args of 
-    ("-v":_) -> solveIO game
-    _ -> return $ execState solve game
+  solved <- if verbose 
+     then solveIO game
+     else return $ execState solve game
   putStrLn (display solved)
